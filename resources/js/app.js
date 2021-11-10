f8b54b3af6b9c3cc757828ea28b310c733a54ebc
Adjeri Sabi <sabi@studio-canbe.com>
2021-11-10 15:21:40 +0900

    chat ui update

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue').default;
+window.Vue = require("vue").default;
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +19,17 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+
+//Add these two components.
+Vue.component(
+    "chat-messages",
+    require("./components/ChatMessages.vue").default
+);
+Vue.component("chat-form", require("./components/ChatForm.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +38,31 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    //Store chat messages for display in this array.
+    data: {
+        messages: [],
+    },
+    //Upon initialisation, run fetchMessages().
+    created() {
+        this.fetchMessages();
+    },
+    methods: {
+        fetchMessages() {
+            //GET request to the messages route in our Laravel server to fetch all the messages
+            axios.get("/messages").then((response) => {
+                //Save the response in the messages array to display on the chat view
+                this.messages = response.data;
+            });
+        },
+        //Receives the message that was emitted from the ChatForm Vue component
+        addMessage(message) {
+            //Pushes it to the messages array
+            this.messages.push(message);
+            //POST request to the messages route with the message data in order for our Laravel server to broadcast it.
+            axios.post("/messages", message).then((response) => {
+                console.log(response.data);
+            });
+        },
+    },
 });
